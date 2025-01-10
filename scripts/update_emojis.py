db196345addbff27715ecedc7b6049a7c5f0b72f
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-01-10 13:49:35 +0100

    Update emojis used for validation (#10149)
    ## Describe your changes
    
    We use a set of all existing emojis to validate emoji usage across
    various elements. This collection needs routine updates whenever new
    emojis are released.
    
    This PR also includes a small fix to get the `update_emojis.py` script
    running again with the latest version of `emoji` library.
    
    Emojis before update: 5034
    Emojis after update:  5042
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- scripts/update_emojis.py
+++ scripts/update_emojis.py
@@ -22,7 +22,7 @@ This script requires the emoji package to be installed: pip install emoji.
 import os
 import re
 
-from emoji.unicode_codes.data_dict import EMOJI_DATA
+from emoji.unicode_codes import EMOJI_DATA
 from streamlit.emojis import ALL_EMOJIS
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
